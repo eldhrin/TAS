8d2e7cea71a41148fc70ed99e786ce2c3c09f247
eldhrin <negative1764@gmail.com>
2019-03-01 17:37:44 +0000

    .

--- src/tas/TAS.java
+++ src/tas/TAS.java
@@ -2,8 +2,10 @@ package tas;
 
 import java.io.IOException;
 import java.text.ParseException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.json.*;
+import org.xml.sax.SAXException;
 
 
 
@@ -18,12 +20,16 @@ public class TAS {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException, InvalidFormatException, ParseException {
+    public static void main(String[] args) throws IOException, InvalidFormatException, ParseException, ParserConfigurationException, SAXException, TransformerException {
         
-        //GetXlsx.getXlsx();
-        //WriteXlsx.writeXlsx();
-        //GenerateXlsx.generateXlsx();
-        GetHols.getHols();
+        //SWLtoDB.SWLtoDB();
+         //DBtoXLSX.DBtoXLSX();
+         //GetHols.getHols();
+//        WriteReport.writeReport();
+       // XLSXtoDB.xlsxtoDB();
+        //ReadXml.readXml();
+       // AwardstoDB.awardstoDB();
+        Research.research();
     }
     
 }
